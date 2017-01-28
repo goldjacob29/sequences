43118a0d910d902346fbc23a3cd462583bbe8b5c
Jacob Gold <goldjacob29@uchicago.edu>
2017-01-27 18:16:38 -0600

    Just need to figure out how to add things to list instead of replace

--- app/controllers/sequence_controller.rb
+++ app/controllers/sequence_controller.rb
@@ -1,6 +1,16 @@
 class SequenceController <ApplicationController
 
   def all_guesses
+
+    @first_number = params["first_number"].to_i
+    @second_number = params["second_number"].to_i
+    @third_number = params["third_number"].to_i
+    @res = ""
+    if @first_number < @second_number && @second_number < @third_number
+      @res = "Yes!"
+    else
+      @res = "No."
+    end
     render("sequence/all_guesses.html.erb")
   end
 
